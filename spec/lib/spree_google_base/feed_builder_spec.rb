e383c56e34e5b3dd387a6b0ae8eaf2cfd91f906a
Tsyren Ochirov <nsu1team@gmail.com>
2014-10-10 01:50:00 +0200

    Slug instead of permalink.

--- spec/lib/spree_google_base/feed_builder_spec.rb
+++ spec/lib/spree_google_base/feed_builder_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe SpreeGoogleBase::FeedBuilder do
-  
+
   describe 'as class' do
     context '#builders should return an array for each store' do
       Spree::Store.delete_all
@@ -14,119 +14,119 @@ describe SpreeGoogleBase::FeedBuilder do
   end
 
   describe 'as instance' do
-    before{ @output = '' } 
+    before{ @output = '' }
     describe 'in general' do
       before(:each) do
         Spree::GoogleBase::Config.set(:public_domain => 'http://mydomain.com')
         Spree::GoogleBase::Config.set(:store_name => 'Froggies')
-       
+
         @builder = SpreeGoogleBase::FeedBuilder.new
         @xml = Builder::XmlMarkup.new(:target => @output, :indent => 2, :margin => 1)
         @product = Factory(:product)
       end
-      
+
       it 'should include products in the output' do
         @builder.build_product(@xml, @product)
-        
+
         @output.should include(@product.name)
-        @output.should include("products/#{@product.permalink}")
+        @output.should include("products/#{@product.slug}")
         @output.should include(@product.price.to_s)
       end
-      
+
       it 'should build the XML and not bomb' do
         @builder.generate_xml @output
-        
+
         @output.should =~ /#{@product.name}/
         @output.should =~ /Froggies/
       end
-      
+
     end
-  
+
     describe 'w/ store defined' do
       before(:each) do
         @store = Factory :store, :domains => 'www.mystore.com', :code => 'first', :name => 'Goodies, LLC'
         @store2 = Factory :store, :domains => 'www.anotherstore.com', :code => 'second', :name => 'Gifts, LLC'
         @builder = SpreeGoogleBase::FeedBuilder.new(:store => @store)
       end
-      
+
       it "should know its path relative to the store" do
         @builder.path.should == "#{::Rails.root}/tmp/google_base_v#{@store.code}.xml"
       end
-      
+
       it "should initialize with the correct domain" do
-        @builder.domain.should == @store.domains.match(/[\w\.]+/).to_s 
+        @builder.domain.should == @store.domains.match(/[\w\.]+/).to_s
       end
-      
+
       it "should initialize with the correct scope" do
         @builder.ar_scope.to_sql.should == Spree::Product.by_store(@store).google_base_scope.scoped.to_sql
       end
-      
+
       it "should initialize with the correct title" do
         @builder.title.should == @store.name
       end
-      
+
       it 'should include stores meta' do
         @xml = Builder::XmlMarkup.new(:target => @output, :indent => 2, :margin => 1)
         @product = Factory(:product)
-        
+
         @builder.build_meta(@xml)
-        
+
         @output.should =~ /#{@store.name}/
         @output.should =~ /#{@store.domains}/
       end
-      
+
       it 'should include only the right products' do
         @xml = Builder::XmlMarkup.new(:target => @output, :indent => 2, :margin => 1)
         needed_product = Factory(:product, :stores => [@store])
         wrong_product = Factory(:product, :stores => [@store2], :name => 'This does NOT belong in the first store')
-        
+
         @builder.generate_xml @output
         @output.should =~ /#{needed_product.name}/
         @output.should_not =~ /#{wrong_product.name}/
       end
     end
-    
+
     describe 'w/out stores' do
-      
+
       before(:each) do
         Spree::GoogleBase::Config.set(:public_domain => 'http://mydomain.com')
         Spree::GoogleBase::Config.set(:store_name => 'Froggies')
-        
+
         @builder = SpreeGoogleBase::FeedBuilder.new
       end
-      
+
       it "should know its path" do
         @builder.path.should == "#{::Rails.root}/tmp/google_base_v.xml"
       end
-      
+
       it "should initialize with the correct domain" do
         @builder.domain.should == Spree::GoogleBase::Config[:public_domain]
       end
-      
+
       it "should initialize with the correct scope" do
         @builder.ar_scope.to_sql.should == Spree::Product.google_base_scope.scoped.to_sql
       end
-      
+
       it "should initialize with the correct title" do
         @builder.title.should == Spree::GoogleBase::Config[:store_name]
       end
-      
+
       it 'should include configured meta' do
         @xml = Builder::XmlMarkup.new(:target => @output, :indent => 2, :margin => 1)
         @product = Factory(:product)
-        
+
         @builder.build_meta(@xml)
-        
+
         @output.should =~ /Froggies/
         @output.should =~ /http:\/\/mydomain.com/
       end
     end
   end
-  
+
   describe 'when misconfigured' do
     it 'should raise an exception' do
       SpreeGoogleBase::FeedBuilder.new.should raise_error
     end
   end
-  
+
 end

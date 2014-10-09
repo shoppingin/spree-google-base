615a6acf693398ca3cc500171616f90402db9727
Tsyren Ochirov <nsu1team@gmail.com>
2014-10-09 14:00:41 +0200

    Clean ups accoding spree2-3 style

--- app/controllers/spree/admin/google_base_settings_controller.rb
+++ app/controllers/spree/admin/google_base_settings_controller.rb
@@ -1,15 +1,15 @@
 class Spree::Admin::GoogleBaseSettingsController < Spree::Admin::BaseController
   helper 'spree/admin/google_base'
-  
+
   def update
     params.each do |name, value|
       next unless Spree::GoogleBase::Config.has_preference? name
       Spree::GoogleBase::Config[name] = value
     end
-    
+
     respond_to do |format|
       format.html {
-        redirect_to admin_google_base_settings_path
+        redirect_to edit_admin_google_base_settings_path
       }
     end
   end
